226e6019b448321103d5bb79ab8575c03f970dff
osman.horasan <osman.h@kayraexport.com>
2023-02-02 14:04:40 +0300

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwaTest';
+  isPush = false
+  ngOnInit() {
+    this.pushController()
+  }
+
+  pushController() {
+    if (Notification.permission === "granted") {
+      this.isPush = true
+      this.push()
+    } else {
+      this.pushPer()
+    }
+  }
+
+  push() {
+    // Bildirim API'sine erişebilirseniz
+    if (Notification.permission === "granted") {
+      // Bildirim objesi oluşturun
+      const notification = new Notification("Title", {
+        body: "Notification Content",
+        icon: "assets/icons/icon-128x128.png"
+      });
+    }
+  }
+
+  pushPer() {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        this.isPush = true
+        this.push()
+      } else {
+        this.isPush = false
+      }
+    });
+  }
 }
